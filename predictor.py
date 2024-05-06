0ed83dbf105fd38345f4852b1d452437f375d391
DS-1090 <126580400+DS-1090@users.noreply.github.com>
2024-05-06 13:15:02 +0530

    Add files via upload

---
+++ predictor.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request, jsonify
+import pickle
+import numpy as np
+
+app = Flask(__name__)
+model = pickle.load(open('dectree_model.pkl','rb'))
+
+@app.route('/')
+def home():
+    return render_template('webpage.html')
+@app.route('/predict',methods=['POST'])
+def predict():
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+    output = round(prediction[0], 2)
+    print(prediction)
+    print(final_features)
+    return render_template('webpage.html', prediction_text='The predicted price of the house is {} Lakhs'.format(output))
+
+if __name__ == "__main__":
+    app.run(debug=True)
