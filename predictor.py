55e5db9877ac20ac35038d995ff85bbcfedc4eaf
DS-1090 <126580400+DS-1090@users.noreply.github.com>
2024-05-07 15:19:42 +0530

    Add files via upload

--- predictor.py
+++ predictor.py
@@ -3,7 +3,7 @@ import pickle
 import numpy as np
 
 app = Flask(__name__)
-model = pickle.load(open('dectree_model.pkl','rb'))
+model = pickle.load(open('forest_model.pkl','rb'))
 
 @app.route('/')
 def home():
